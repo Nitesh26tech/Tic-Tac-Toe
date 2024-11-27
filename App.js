e886a8a3632a57714e3c0877febc7cbf6d844486
Nitesh26tech <niteshgurjar08087@gmail.com>
2024-11-27 15:26:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Box from './component/box';
+
+function App() {
+  return (
+    <div className="container">
+      <Box />
+    </div>
+  );
+}
+
+export default App;
